b6c518bafd6589f0ce100f135bd235e80e654f4a
tiendat1104 <57142737+faregas2007@users.noreply.github.com>
2021-06-10 00:16:41 +0200

    Update rundeltabt.py

--- core/rundeltabt.py
+++ core/rundeltabt.py
@@ -33,11 +33,15 @@ function_dir = sys.argv[2]
 index_in = int(sys.argv[3])
 #dia_index = sys.argv[3]
 
-dia_index = [2, 3, 5, 7, 9, 11, 12, 13, 14]
+dia_index = [2, 3, 5, 7, 9, 11, 12, 13, 14, 15]
 sys.path.insert(0, function_dir)
 
+	
 def num_integrate(index_in):
-	fcn = 'ampibp_total_dia'+str(dia_index[index_in])+'.py'
+	if (index_in == 9):
+		fcn = 'ampibp_total_'+'CT'+'.py'
+	else:
+		fcn = 'ampibp_total_dia'+str(dia_index[index_in])+'.py'
 	module = fcn.strip('.py')
 	fcn_module = import_module(module)
 	ibpptotal = getattr(fcn_module, 'ibpptotal')
@@ -51,26 +55,33 @@ def num_integrate(index_in):
 	fin = []
 	finerr = []
 
-	for i in range(2):
-		start = timer()
-		integ(ibpptotal(dia_index[index_in], i), nitn=iters, neval=startdiv, alpha=alphafix)
-		result = integ(ibpptotal(dia_index[index_in], i), nitn=iters, neval=enddiv, alpha=alphafix)
-		end = timer()
-		pole.append(corrf*result.mean)
-		poleerr.append(corrf*result.sdev)
+	if (index_in == 9):
+		for i in range(2):
+			pole.append(ibpptotal(i))
+			poleerr.append(ibpptotal(i))
+			fin.append(ibpftotal(i))
+			finerr.append(ibpftotal(i))
+	else:
+		for i in range(2):
+			start = timer()
+			integ(ibpptotal(i), nitn=iters, neval=startdiv, alpha=alphafix)
+			result = integ(ibpptotal(i), nitn=iters, neval=enddiv, alpha=alphafix)
+			end = timer()
+			pole.append(result.mean)
+			poleerr.append(result.sdev)
 
-	for i in range(2):
-		start = timer()
-		integ(ibpftotal(dia_index[index_in], i), nitn=iters, neval=startfin, alpha=alphafix)
-		result = integ(ibpftotal(dia_index[index_in], i), nitn=iters, neval=endfin, alpha=alphafix)
-		end = timer()
+		for i in range(2):
+			start = timer()
+			integ(ibpftotal(i), nitn=iters, neval=startfin, alpha=alphafix)
+			result = integ(ibpftotal(i), nitn=iters, neval=endfin, alpha=alphafix)
+			end = timer()
 			
-		if(index_in == 0 or index_in == 6 or index_in == 8):
-			fin.append(corrf*result.mean)
-			finerr.append(corrf*result.sdev)
+			if(index_in == 0 or index_in == 6 or index_in == 8):
+				fin.append(corrf*result.mean)
+				finerr.append(corrf*result.sdev)
 
-		fin.append(result.mean)
-		finerr.append(result.sdev)
+			fin.append(result.mean)
+			finerr.append(result.sdev)
 	
 	return {'ReDivAvg':fin[0], 'ReDivErr':finerr[0] ,'ImDivAvg':fin[1], 'ImDivErr':finerr[1], 'ReDivAvg':pole[0], 'ReDivErr':poleerr[0],'ImDivAvg':pole[1], 'ImDivErr':poleerr[1]}
 
@@ -95,7 +106,13 @@ def csvwrite(name, index_in):
 		squarks = 'stop'
 	else(squark == 2):
 		squarks = 'sbot'
-	rows = [[squarks, str(tanb), str(mA), who, str(DDS), who, str(ReFinAvg), str(ReFinErr), str(ReDivAvg), str(ReDivErr), str(ImFinAvg), str(ImFinErr), str(ImDivAvg), str(ImDivErr), str(Lambda), str(eps)]]
+		
+	if (index_in == 9):
+		filename = 'CT'+sys.argv[1]+'.csv'
+		rows = [[squarks, str(tanb), str(mA), who, str(DDS), who, str(ReFinAvg), str(ReFinErr), str(ReDivAvg), str(ReDivErr), str(ImFinAvg), str(ImFinErr), str(ImDivAvg), str(ImDivErr), str(Lambda), str(eps)]]
+	else:
+		filename = str(diagrams)+sys.argv[1]+'.csv'
+		rows = [[squarks, str(tanb), str(mA), who, str(DDS), who, str(ReFinAvg), str(ReFinErr), str(ReDivAvg), str(ReDivErr), str(ImFinAvg), str(ImFinErr), str(ImDivAvg), str(ImDivErr), str(Lambda), str(eps)]]
 	with open(filename, 'w') as csvfile:
 		csvwriter = csv.writer(csvfile)
 		csvwriter.writerow(fields)
