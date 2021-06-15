2ffb65a9dedf193d9b69bb4016a97e46394f8751
tiendat1104 <57142737+faregas2007@users.noreply.github.com>
2021-06-15 12:31:32 +0200

    Update run.py

--- core/run.py
+++ core/run.py
@@ -20,7 +20,7 @@ enddiv = islha_obj.blocks['VEGAS'][3]
 startfin =islha_obj.blocks['VEGAS'][4]
 endfin = islha_obj.blocks['VEGAS'][5]
 alphafix = islha_obj.blocks['VEGAS'][6]
-integrator = islha_obj.blocks['VEGAS'][7]
+integrator = str(islha_obj.blocks['VEGAS'][7])
 eps = islha_obj.blocks['IBPPARA'][2]
 corrf = islha_obj.blocks["REAL"][7]
 
